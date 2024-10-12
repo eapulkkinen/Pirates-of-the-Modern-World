936ba8cf5d4eaa825cfd702f2b19866d95c56128
Vili Sihvo <visihvo@student.jyu.fi>
2024-10-12 21:12:03 +0300

    Poistettu App.jsx turhat importit. Tehty myös karttapisteiden´lisäys kartalle toimimaan kovakoodatuilla karttapisteillä. Yritetty kommentoida rivejä, jotta ohjelman toiminta olisi selkeämpää...

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import L from 'leaflet'
-import './App.css'
-import Map from './components/Map'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Map from './components/Map';
 
 
 function App() {
+  const [koordinaattiLista, setKoordinaatit] = useState([]);
+
+  const haeKoordinaatit = () => {
+    //Oikeasti tässä kohtaa haettaisi databasesta tmv. koordinaatit
+    
+    //Tässä kovakoodattu esimerkki listasta
+    const koordinaatit = [
+      {
+        coords: [62.2391, 25.7387],
+        place: 'Agora'
+      },  
+      {
+        coords: [60.1699, 24.9384],
+        place: 'Helsinki'
+      }
+    ];
+
+    //const koordinaatit = []; //Toimii myös tyhjällä listalla eli 
+    //"kun ei ole valittu yhtään suodattimia yms ei näytetä mtn"
+
+    setKoordinaatit(koordinaatit); //Map.jsx "kutsu"
+  };
+
+  useEffect(() => {
+    haeKoordinaatit();
+  }, []);
 
   return (
     <>
@@ -15,14 +38,14 @@ function App() {
           <p>Tänne maatiedot ja suodatusvalinnat?</p>
         </div>
         <div id="karttadiv">
-          <Map></Map>
+          <Map koordinaattiLista={koordinaattiLista} />
         </div>
         <div id="oikeadiv" className="sivudiv">
           <p>Tänne tietoa tapahtumista?</p>
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,26 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import L from 'leaflet';    //Leafletin perusominaisuudet
 import 'leaflet/dist/leaflet.css'; //Leaflet CSS
 
-const Map = () => {
-  useEffect(() => {     
-    //setView [] sisään koordinaatit kartan keskityspisteeksi ja luku sen jälkeen on zoomin määrä
-    const map = L.map('karttadiv').setView([20, 0], 2);
+const Map = ({ koordinaattiLista }) => {
+    const [map, setMap] = useState(null);
+    useEffect(() => {     
+        //setView [] sisään koordinaatit kartan keskityspisteeksi ja luku sen jälkeen on zoomin määrä
+        const initMap = L.map('karttadiv').setView([20, 0], 2);
 
-    // tileLayerin parametreja muuttamalla saa vaihdettua kartan tyyppiä
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-      attribution: '&copy; <a href="https://carto.com/">CartoDB</a>',
-    }).addTo(map);
+        // tileLayerin parametreja muuttamalla saa vaihdettua kartan tyyppiä
+        L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+        attribution: '&copy; <a href="https://carto.com/">CartoDB</a>',
+    }).addTo(initMap);
+
+    setMap(initMap);
 
     return () => {
-      map.remove();
+        initMap.remove();
     };
-  }, []);
+    }, []);
 
-  return (
-    <div id="karttadiv">
-      {}
-    </div>
+    useEffect(() => {
+        if (map && koordinaattiLista.length > 0) {
+            koordinaattiLista.forEach(koordinaatit => {
+                L.marker(koordinaatit.coords)
+                .addTo(map)
+                .bindPopup(koordinaatit.place);
+            });
+        }
+    }, [map, koordinaattiLista]);
+
+    return (
+        <div id="karttadiv">
+        {}
+        </div>
   );
 };
 
