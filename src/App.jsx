d5237830ac01c6ded28ee3c9b6f4d996d2387a31
Valtteri Sivula <valtteri15.sivula@gmail.com>
2024-12-03 21:51:25 +0200

    kysymysmerkin testiversio

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import country_codes from './data/country_codes';
 import pirate_attacks from './data/pirate_attacks';
 import country_indicators from './data/country_indicators';
 import Modal from './components/Modal/Modal'
+import Questio_Mark from './components/Question_Mark';
 
 
 function App() {
---
+++ src/components/Question_Mark.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import '../index.css';
+
+const Questio_Mark = () => {
+    return (
+        <div class="tooltip-container">
+        <div class="question-mark">?</div>
+        <div class="tooltip">
+        <span class="tooltip-text">
+        Exclusive Economic Zone (EEZ) is an area
+        of the sea in which a sovereign state has
+        exclusive rights regarding the exploration
+        and use of marine resources.
+        </span>
+        </div>
+        </div>
+    );
+}
+
+export default Questio_Mark;
